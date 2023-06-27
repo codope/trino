d0ff3cac51acecadae2ce1fd6fe31a5586df3748
Sagar Sumit <sagarsumit09@gmail.com>
2023-06-27 22:16:28 +0530

    Add missing license header
    Fix NPE

--- plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/HudiAvroPageSource.java
+++ plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/HudiAvroPageSource.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.trino.plugin.hudi;
 
 import io.airlift.log.Logger;
@@ -122,7 +135,7 @@ public class HudiAvroPageSource
                 appendTo(columnTypes.get(column), record.getData(), output);
             }
         }
-        return null;
+        return pageBuilder.build();
     }
 
     private void appendTo(Type type, Object value, BlockBuilder output)
